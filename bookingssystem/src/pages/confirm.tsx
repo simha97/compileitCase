b8bd3bcb263a0d22e3a7932b41e036a241d3195f
simha97 <62260118+simha97@users.noreply.github.com>
2025-06-09 13:36:40 +0200

    added confirm page and UPDATE to supabase

--- bookingssystem/src/pages/confirm.tsx
+++ bookingssystem/src/pages/confirm.tsx
@@ -1,7 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import { useBooking } from "@/context/BookingContext";
+import { supabase } from "@/lib/supabaseClient";
 
-function confirm() {
-  return <div></div>;
+export default function Confirm() {
+  const [name, setName] = useState("");
+  const { selectedSlotId } = useBooking();
+
+  const handleConfirm = async () => {
+    const { error } = await supabase
+      .from("slots")
+      .update({
+        booked: true,
+        booked_by: name,
+      })
+      .eq("id", selectedSlotId)
+      .eq("booked", false);
+
+    if (error) {
+      console.error("Error booking slot:", error);
+      alert("Failed to book slot");
+    } else {
+      console.log(`Booking confirmed for ${name} at slotId: ${selectedSlotId}`);
+    }
+  };
+
+  return (
+    <div className=" flex flex-col items-center justify-items-center min-h-screen p-6 pb-13 gap-16 sm:p-20">
+      <h1 className="text-[40px] leading-[1] ">Vem bokar?</h1>
+
+      <div>
+        <label htmlFor="name" className="text-xl font-bold">
+          Förnamn och efternamn
+        </label>
+        <input
+          id="name"
+          type="text"
+          placeholder="Skriv ditt fullständiga namn här"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          className="border border-stone-300 w-full text-s rounded-lg p-2"
+        />
+      </div>
+
+      <button
+        onClick={handleConfirm}
+        className="mt-auto w-full rounded-2xl bg-stone-900 p-3.5 text-white text-center"
+      >
+        Boka
+      </button>
+    </div>
+  );
 }
-
-export default confirm;
